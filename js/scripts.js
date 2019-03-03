91dfd5ad66c5689c287d41c4e6b0ca82080d639c
nurwebnur <nur.web.nur@gmail.com>
2019-03-03 18:53:51 +0600

    end scroll

--- js/scripts.js
+++ js/scripts.js
@@ -161,4 +161,11 @@ $(function() {
                 .removeClass("active");
         });
     });
+
+    $(".header a").click(function() {
+        var link = $(this).attr("href");
+        var coordinats = $(link).offset().top;
+        $("html, body").animate({ scrollTop: coordinats }, 1000);
+        return false;
+    });
 });
